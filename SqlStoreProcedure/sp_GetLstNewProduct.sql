bd3ce3c9d26474c2bad594e57716fbf45b04707b
DzungHT <dzunght95@gmail.com>
2016-11-04 00:20:01 +0700

    add new sp_GetLstNewProduct.sql

---
+++ SqlStoreProcedure/sp_GetLstNewProduct.sql
@@ -0,0 +1,27 @@
+﻿SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		DzungHT
+-- Create date: Ngày 03/11/2016
+-- Description:	Lấy danh sách sản phẩm mới
+-- =============================================
+CREATE PROCEDURE sp_GetLstNewProduct
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    -- Insert statements for procedure here
+	SELECT TOP (120)
+		[M].*
+	FROM
+		[M1Product] [M]
+	WHERE
+		[M].[Status] = 1 AND
+		[M].[Showhome] = 1
+		
+END
+GO
